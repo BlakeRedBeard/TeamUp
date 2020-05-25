3163dbfa17229dde49a20d033b1d1e41ca891629
alfonso <alfonso>
2020-05-25 18:18:14 +0200

    add interface

---
+++ Ing. Project/Script.js
@@ -0,0 +1,9 @@
+
+function Registrati() {
+	window.location.href = "http://localhost/project/Ing.%20Project/Registrazione.html";
+}
+
+function login() {
+	window.location.href = "http://localhost/project/Ing.%20Project/index.html";
+}
+
